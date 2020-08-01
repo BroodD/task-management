1cb98f874d66abdc77451ff859d429ccdf56c64e
BroodD <svyatddev@gmail.com>
2020-08-01 16:44:33 +0300

    change server url:

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class BaseHttpService {
-	BASE_URL = 'https://nestjs-taskmanagement.herokuapp.com';
+	BASE_URL = process.env.REACT_APP_SERVER_URL;
 	_accessToken = null;
 
 	constructor(routerStore) {
